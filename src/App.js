8bbfe5af7a5c05743aff864083e1260e8d88cb1e
Flavio Rocha <fgustavo.rocha@hotmail.com>
2023-09-08 23:46:05 -0300

    criando a v0 do quiz show

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import logo from './logo.svg';
+import React, { useContext, useState, createContext, useEffect } from 'react'
 import './App.css';
 
-function App() {
+const Context = createContext(0);
+let correctAnswer = 2;
+
+function Question({ statement }){
+  return(
+    <div className='Question-Area'>{statement}</div>
+  )
+}
+
+function Answer({ optionNumber, alternative, answerResult }){
+  let [choicedOption, setChoicedOption] = useContext(Context);
+
+  const optionClass = () => {
+    if (!answerResult && choicedOption === optionNumber){
+      return 'Clicked';
+    }
+
+    else if ((!answerResult && choicedOption !== optionNumber) || answerResult.option !== optionNumber){
+      return '';
+    }
+
+    else {
+      return answerResult.final;
+    }
+  }
+  const onClickOption = () => {
+    setChoicedOption(optionNumber);
+    // setTimeout(() => {
+      
+    // }, 1000)
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='Answer-Component'>
+        <div className={`Answer-Option ${optionClass()}`} onClick={onClickOption}>{optionNumber}</div>
+        <div className={`Answers ${answerResult.option === optionNumber ? answerResult.final : ''}`}>{alternative}</div>
+      </div>
+    </>
+
+  )
+}
+
+function Reward ({Result, Prize}) {
+  return (
+    <>
+      <div>
+        <div className='Reward-Label'>{Result}:</div>
+        <div className='Reward'>{Prize}</div>
+      </div>
+    </>
+  )
+}
+
+function App() {
+  let [choicedOption, setChoicedOption] = useState(0);
+  let [result, setResult] = useState("");
+
+  useEffect(() => {
+    if (choicedOption !== 0){
+      // Como a aparição da cor na opção selecionada sofre um pequeno delay, é colocado este setTimeout para esperar até que a
+      // cor amarela seja devidamente colocada na resposta.
+      setTimeout(() => { 
+        if (window.confirm("Você está certo disso?")){
+          if (choicedOption === correctAnswer){
+            setResult({
+              option: choicedOption,
+              final: "Correct"
+            });
+          }
+
+          else {
+            setResult({
+              option: choicedOption,
+              final: "Wrong"
+            })
+          }
+        }
+  
+        else {
+          setChoicedOption(0)
+        }
+      }, 50)
+    }
+    
+  }, [choicedOption])
+  return (
+    <Context.Provider value={[choicedOption, setChoicedOption, result, setResult]}>
+      <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+        <Question statement={"Qual é o planeta do sistema solar que fica mais próximo do Sol?"}/>
+        <Answer optionNumber={1} answerResult={result} alternative={'Vênus'}/>
+        <Answer optionNumber={2} answerResult={result} alternative={'Mercúrio'}/>
+        <Answer optionNumber={3} answerResult={result} alternative={'Júpiter'}/>
+        <Answer optionNumber={4} answerResult={result} alternative={'Netuno'}/>
+        <div className='Rewards-Area'>
+          <Reward Result={'Errar'} Prize={'500'}/>
+          <Reward Result={'Parar'} Prize={'1 mil'}/>
+          <Reward Result={'Acertar'} Prize={'2 mil'}/>
+        </div>
+      </div>
+    </Context.Provider>
   );
 }
 
